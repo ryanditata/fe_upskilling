a374344fd7e0ac6d97749d6ad486c562fbd53dcb
ryanditata <ryanditata38@gmail.com>
2026-04-03 21:39:10 +0700

    get data api faq

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Plus, Minus } from "lucide-react";
+
+type FAQ = {
+  id: number;
+  question: string;
+  answer: string;
+}
+
 const LandingPage = () => {
+  const [faqs, setFaqs] = useState<FAQ[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fatchFAQs();
+  }, []);
+
+  const fatchFAQs = async () => {
+    try {
+      const response = await fetch('${API_URL}/public/faqs}');
+      const result = await response.json();
+
+        if (result.meta.success) setFaqs(result.data);
+        else setError("Tidak dapat memuat FAQ");
+    } catch (err: any) {
+      setError("Terjadi kesalahan saat memuat FAQ:" + err.message);
+    }
+  }
 
   return (
     <div className="font-bold text-2xl">
